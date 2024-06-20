a41e421cdfac427989fe89197841e44f46f325b8
odreystella <odreystella.dev@gmail.com>
2024-06-21 00:32:28 +0900

    feat: User 모델 생성 및 마이그레이션

--- core/models.py
+++ core/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+)
 
-# Create your models here.
+from users.exceptions import EmailRequiredException
+
+
+class Shared(models.Model):
+    """
+    공통으로 사용하는 모델.
+    """
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+
+class UserManager(BaseUserManager):
+    """
+    커스텀 유저 매니저.
+    """
+    def create_user(self, email, password=None, **extra_field):
+        """
+        유저 생성 메서드.
+        """
+        if not email:
+            raise EmailRequiredException
+
+        user = self.model(email=self.normalize_email(email), **extra_field)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+
+class User(AbstractBaseUser, Shared):
+    """
+    커스텀 유저 모델.
+    """
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=255, unique=True)
+
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
